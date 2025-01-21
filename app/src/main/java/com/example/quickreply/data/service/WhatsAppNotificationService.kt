a2c75666ea8d39a2153a07143607370fc1ebb380
Hirahmedkhan <hira.ahmedkhan01@gmail.com>
2025-01-21 12:09:57 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quickreply/data/service/WhatsAppNotificationService.kt
@@ -0,0 +1,22 @@
+package com.example.quickreply.data.service
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+class WhatsAppNotificationService : NotificationListenerService() {
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        if (sbn != null && sbn.packageName == "com.whatsapp") {
+
+            Log.d("WhatsAppService", "Notification from: ${sbn.packageName}")
+
+            val notificationText = sbn.notification.extras.getString("android.text")
+            Log.d("WhatsAppMessage", "Message: $notificationText")
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        Log.d("WhatsAppService", "Notification Removed")
+    }
+}
